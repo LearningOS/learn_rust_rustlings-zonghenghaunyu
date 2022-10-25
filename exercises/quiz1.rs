9c39bd61fe40e665eaf530a682b693edce6ab5a1
zonghenghhuanyu <ywpdms@outlook.com>
2022-10-25 19:00:56 +0800

    下班了

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,7 +8,7 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
 // fn calculate_apple_price {
@@ -16,6 +16,15 @@
 // Don't modify this function!
 #[test]
 fn verify_test() {
+
+    let calculate_apple_price = |price : i32| -> i32 {
+        if(price > 40){
+            price
+        }else{
+            2 * price
+        }
+    };
+
     let price1 = calculate_apple_price(35);
     let price2 = calculate_apple_price(40);
     let price3 = calculate_apple_price(65);
